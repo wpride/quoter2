7a517b543843d6ffd240cb20c66472d4166f6273
wpride <wpride@dimagi.com>
2014-06-08 21:58:34 -0400

    add argument

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
 	return "Hello Derpy World!"
 
+@app.route('/<name>')
+def hello_name(name):
+	return "Hello {} you derp!".format(name)
+
 if __name__ == '__main__':
 	app.run()
