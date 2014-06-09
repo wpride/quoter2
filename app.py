52a1d30a2117e95a4d2125110a04d66a7852b730
wpride <wpride@dimagi.com>
2014-06-08 22:23:54 -0400

    print

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -10,4 +13,5 @@ def hello_name(name):
 	return "Hello {} you derp!".format(name)
 
 if __name__ == '__main__':
+	print os.environ['APP_SETTINGS']
 	app.run()
