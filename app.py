565021930cbcf5b1878ef9d0c80510c352ba6c93
wpride <wpride@dimagi.com>
2014-06-09 18:01:37 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 from flask import Flask
-import os
+from flask.ext.sqlalchemy import SQLAlchemy
+import redis
+from collections import defaultdict
+import operator
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+r_server = redis.Redis('localhost')
 
 @app.route('/')
 def hello():
-	return "Hello Derpy World!"
+    return "Hello Derpy World!"
 
 @app.route('/<name>')
 def hello_name(name):
-	return "Hello {} you derp!".format(name)
+
+    splitstring = name.split('_')
+    accumulator = ""
+    countmap=defaultdict(int)
+
+    for splitsegment in splitstring:
+        mclips = r_server.sinter("tag:"+splitsegment)
+        accumulator += splitsegment
+        for elem in mclips:
+            countmap[elem] += 1
+
+    result = max(countmap.iteritems(), key=lambda x: x[1])
+
+    clip_key = result[0]
+
+    clip_path = r_server.get("clip:"+clip_key)
+
+    return "<a href='https://www.youtube.com/watch?v={}'>Clip</a>".format(clip_path)
 
 if __name__ == '__main__':
-	print os.environ['APP_SETTINGS']
 	app.run()
